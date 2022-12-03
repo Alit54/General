948e3ab5180d42ac2f6a5e95e15e88596f42e4b5
Simone Alessandro Casciaro <95073723+Alit54@users.noreply.github.com>
2022-12-03 16:45:38 +0100

    Added function SplitStrings

--- gofunc/string.go
+++ gofunc/string.go
@@ -11,3 +11,20 @@ func SortString(w string) string {
 	sort.Strings(s)
 	return strings.Join(s, "")
 }
+
+// Returns a slice of strings divided in n parts
+/*func SplitEqually(s string, n int) []string {
+	slice := make([]string, 0)
+	for i := 0; i < n; i++ {
+		slice = append(slice, s)
+	}
+	return slice
+}*/
+
+// Returns a slice of strings divided in the index position. Value in the index position will be in the second part.
+func SplitStrings(s string, index int) []string {
+	slice := make([]string, 0)
+	slice = append(slice, s[:index])
+	slice = append(slice, s[index:])
+	return slice
+}
